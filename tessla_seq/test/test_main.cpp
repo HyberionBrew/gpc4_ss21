70d5049ab91d0c70f80d17dbcb84d5207e7598bc
Hannes Hauer <hanneshauer@beeware.at>
2021-05-27 16:20:01 +0000

    Add tests

--- tessla_seq/test/test_main.cpp
+++ tessla_seq/test/test_main.cpp
@@ -1,2 +1,5 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
+
+// Run test cases with:
+// make test TEST_SRC=test/[filename].cpp
--- tessla_seq/test/test_test.cpp
+++ tessla_seq/test/test_test.cpp
@@ -1,14 +1,77 @@
 #include "catch2/catch.hpp"
-#include <Event.h>
-#include <Stream.h>
+#include "../src/Event.h"
+#include "../src/Stream.h"
+#include "../src/Reader.h"
+#include "../src/Writer.h"
+#include "../src/StreamFunctions.h"
 
-TEST_CASE("Tests work") {
-    SECTION("Trivial") {
-        REQUIRE(true == true);
-        REQUIRE(true == false);
+// Usage:
+// make test TEST_SRC=test/test_test.cpp
+
+TEST_CASE("Basic Test Cases") {
+    SECTION("bt_add1") {
+        Reader inReader = Reader("test/data/bt_add1.in");
+        IntStream inStream = inReader.getIntStream("x");
+        Reader outReader = Reader("test/data/bt_add1.out");
+        IntStream intendedResult = outReader.getIntStream("x");
+
+        /* TODO: Implement add
+        IntStream result = add(inStream, 1);
+
+        REQUIRE(result == intendedResult);
+        */
+
     }
 
-    SECTION("Comparison") {
+    SECTION("bt_delay") {
+        Reader inReader = Reader("test/data/bt_delay.in");
+        IntStream delayStreamIn = inReader.getIntStream("d");
+        UnitStream resetStreamIn = inReader.getUnitStream("r");
 
+        Reader outReader = Reader("test/data/bt_delay.out");
+        UnitStream intendedResult = outReader.getUnitStream("y");
+
+        UnitStream result = delay(delayStreamIn, resetStreamIn);
+
+        REQUIRE(result == intendedResult);
     }
+
+    SECTION("bt_last") {
+        Reader inReader = Reader("test/data/bt_last.in");
+        IntStream vStreamIn = inReader.getIntStream("v");
+        UnitStream rStreamIn = inReader.getUnitStream("r");
+
+        Reader outReader = Reader("test/data/bt_last.out");
+        IntStream intendedResult = outReader.getIntStream("y");
+
+        IntStream result = last(vStreamIn, rStreamIn);
+
+        REQUIRE(result == intendedResult);
+    }
+
+    SECTION("bt_merge") {
+        Reader inReader = Reader("test/data/bt_merge.in");
+        IntStream xStreamIn = inReader.getIntStream("x");
+        IntStream yStreamIn = inReader.getIntStream("y");
+
+        Reader outReader = Reader("test/data/bt_merge.out");
+        IntStream intendedResult = outReader.getIntStream("z");
+
+        IntStream result = merge(xStreamIn, yStreamIn);
+
+        REQUIRE(result == intendedResult);
+    }
+
+    SECTION("bt_time") {
+        Reader inReader = Reader("test/data/bt_time.in");
+        IntStream xStreamIn = inReader.getIntStream("x");
+
+        Reader outReader = Reader("test/data/bt_time.out");
+        IntStream intendedResult = outReader.getIntStream("x");
+
+        IntStream result = time(xStreamIn);
+
+        REQUIRE(result == intendedResult);
+    }
+
 }
