e704bec2b6061eadc262b939703c9a362386f571
Hannes Hauer <hanneshauer@beeware.at>
2021-05-27 16:18:09 +0000

    Add comparison operator to Event structs

--- tessla_seq/src/Event.h
+++ tessla_seq/src/Event.h
@@ -10,11 +10,19 @@
 
 typedef struct UnitEvent{
     size_t timestamp;
+
+    bool operator==(UnitEvent const & rhs) const {
+        return this->timestamp == rhs.timestamp;
+    }
 } UnitEvent;
 
 typedef struct IntEvent{
     size_t timestamp;
     int32_t value;
+
+    bool operator==(IntEvent const & rhs) const {
+        return (this->timestamp == rhs.timestamp) && (this->value == rhs.value);
+    }
 } IntEvent;
 
 #endif //GPU_TESSLA_EVENT_H
