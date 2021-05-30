f3f686b2d75e00479e45beff2e8e017e5507c6ea
TheBitFighter <fabian@theposch.dev>
2021-05-30 15:14:22 +0200

    Decode and Instruction interface implemented. Untested.

---
+++ vm/src/InstrInterface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fabian on 30/05/2021.
+//
+
+#include "InstrInterface.h"
---
+++ vm/src/InstrInterface.h
@@ -0,0 +1,14 @@
+//
+// Created by fabian on 30/05/2021.
+//
+
+#ifndef GPC4_SS21_INSTRINTERFACE_H
+#define GPC4_SS21_INSTRINTERFACE_H
+
+
+class InstrInterface {
+
+};
+
+
+#endif //GPC4_SS21_INSTRINTERFACE_H
