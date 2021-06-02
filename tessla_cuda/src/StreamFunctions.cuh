0cc6344a3644791cb9c8a2e9d9bce4d77e068a3e
Hannes Hauer <hannes.hauer@student.tuwien.ac.at>
2021-06-02 10:28:28 +0000

    Add test infrastructure to CUDA

--- tessla_cuda/src/StreamFunctions.cuh
+++ tessla_cuda/src/StreamFunctions.cuh
@@ -4,6 +4,7 @@
 
 #ifndef TESSLA_CUDA_STREAMFUNCTIONS_CUH
 #define TESSLA_CUDA_STREAMFUNCTIONS_CUH
+#include <cuda_runtime.h>
 #include "Stream.cuh"
 // time operation for unit stream
 void time(IntStream *s, IntStream *result, cudaStream_t stream);
@@ -12,5 +13,5 @@ void delay(IntStream *s, UnitStream *r, UnitStream*result, cudaStream_t stream);
 __global__ void time_cuda(int* input_timestamp, int* output_timestamps, int* output_values, int size, int*offs, int* resultOffse);
 __global__ void last_cuda(int* block_red, int* input_timestamp, int* input_values,int*unit_stream_timestamps,  int* output_timestamps, int* output_values,int IntStreamSize, int size, int* offsInt, int* offsUnit);
 __global__ void final_reduce(int* block_red,int size,int* offset);
-__global__ void delay
+//__global__ void delay
 #endif //TESSLA_CUDA_STREAMFUNCTIONS_CUH
---
+++ tessla_cuda/test/test_cuda.cpp
@@ -0,0 +1,67 @@
+#include <string.h>
+#include "../../test/catch2/catch.hpp"
+#include "../src/Stream.cuh"
+#include "../src/StreamFunctions.cuh"
+
+
+TEST_CASE("Basic tests") {
+    SECTION("time()") {
+        int size = 4;
+        int sizeAllocated = (size_t) size * sizeof(int);
+
+        int inTimestamps[4];
+        int inInts[4];
+        int outTimestamps[4];
+        int outInts[4];
+
+        inTimestamps[0] = 2;
+        inTimestamps[1] = 5;
+        inTimestamps[2] = 6;
+        inTimestamps[3] = 9;
+
+        inInts[0] = 1;
+        inInts[1] = 3;
+        inInts[2] = 9;
+        inInts[3] = 4;
+
+        outTimestamps[0] = 2;
+        outTimestamps[1] = 5;
+        outTimestamps[2] = 6;
+        outTimestamps[3] = 9;
+
+        outInts[0] = 2;
+        outInts[1] = 5;
+        outInts[2] = 6;
+        outInts[3] = 9;
+
+        int *host_timestampOut = (int *) malloc(size * sizeof(int));
+        int *host_valueOut = (int*) malloc(size * sizeof(int));
+        memset(host_timestampOut, 0, sizeAllocated);
+        memset(host_valueOut, 0, sizeAllocated);
+
+        IntStream inputStream(inTimestamps, inInts, size);
+        IntStream outputStream(host_timestampOut, host_valueOut, size);
+
+        inputStream.copy_to_device();
+        outputStream.copy_to_device();
+
+        time(&inputStream, &outputStream, 0);
+
+        outputStream.copy_to_host();
+
+        REQUIRE(outputStream.host_timestamp[0] == outTimestamps[0]);
+        REQUIRE(outputStream.host_values[0] == outInts[0]);
+        REQUIRE(outputStream.host_timestamp[1] == outTimestamps[1]);
+        REQUIRE(outputStream.host_values[1] == outInts[1]);
+        REQUIRE(outputStream.host_timestamp[2] == outTimestamps[2]);
+        REQUIRE(outputStream.host_values[2] == outInts[2]);
+        REQUIRE(outputStream.host_timestamp[3] == outTimestamps[3]);
+        REQUIRE(outputStream.host_values[3] == outInts[3]);
+
+        // Cleanup
+        inputStream.free_device();
+        outputStream.free_device();
+        free(host_valueOut);
+        free(host_timestampOut);
+    }
+}
