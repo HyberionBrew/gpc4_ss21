2d9263742999c0ba4156ec80907841cbf0e3c858
Klaus Kraßnitzer <klaus.krassnitzer@student.tuwien.ac.at>
2021-06-14 23:31:17 +0200

    shared_ptr refactor

--- arc/tessla_seq/src/StreamFunctions.h
+++ arc/tessla_seq/src/StreamFunctions.h
@@ -11,26 +11,27 @@
 #include <iterator>
 #include <cstdint>
 #include <iostream>
+#include <memory>
 
 // === BASIC OPERATIONS ===
 
 // returns a new stream with unit event at time 0
-UnitStream unit();
+std::shared_ptr<UnitStream> unit();
 
 // returns a new stream with int event at time 0
-IntStream def(int32_t val);
+std::shared_ptr<IntStream> def(int32_t val);
 
 // time operation
-IntStream time(Stream& s);
+std::shared_ptr<IntStream> time(Stream& s);
 
 // last operation (sample v at ticks of r)
-IntStream last(IntStream v, Stream& r);
+std::shared_ptr<IntStream> last(IntStream& v, Stream& r);
 
 // delay operation (delay events of d on ticks of r and close transitively)
-UnitStream delay(IntStream d, Stream& r);
+std::shared_ptr<UnitStream> delay(IntStream& d, Stream& r);
 
 // count operation (running counter of unit events on x)
-IntStream count(UnitStream x);
+std::shared_ptr<IntStream> count(UnitStream& x);
 
 /**
  * Merges 2 input streams x and y, if oth events contain an event at the same timestamp,
@@ -39,52 +40,52 @@ IntStream count(UnitStream x);
  * @param y
  * @return a new merged stream
  **/
-IntStream merge(IntStream x, IntStream y);
+std::shared_ptr<IntStream> merge(IntStream& x, IntStream& y);
 
 // merge two unit streams
-UnitStream merge(UnitStream x, UnitStream y);
+std::shared_ptr<UnitStream> merge(UnitStream& x, UnitStream& y);
 
 // === ARITHMETIC OPERATIONS ===
 
 // stream + value
-IntStream add(IntStream x, int value);
+std::shared_ptr<IntStream> add(IntStream& x, int value);
 
 // stream - value
-IntStream sub1(IntStream x, int value);
+std::shared_ptr<IntStream> sub1(IntStream& x, int value);
 
 // value - stream
-IntStream sub2(IntStream x, int value);
+std::shared_ptr<IntStream> sub2(IntStream& x, int value);
 
 // stream * value
-IntStream mul(IntStream x, int value);
+std::shared_ptr<IntStream> mul(IntStream& x, int value);
 
 // stream / value
-IntStream div1(IntStream x, int value);
+std::shared_ptr<IntStream> div1(IntStream& x, int value);
 
 // value / stream
-IntStream div2(IntStream x, int value);
+std::shared_ptr<IntStream> div2(IntStream& x, int value);
 
 // stream % value
-IntStream mod1(IntStream x, int value);
+std::shared_ptr<IntStream> mod1(IntStream& x, int value);
 
 // value % stream
-IntStream mod2(IntStream x, int value);
+std::shared_ptr<IntStream> mod2(IntStream& x, int value);
 
 // all two stream operations are SLIFT
 
 // x + y
-IntStream add(IntStream x, IntStream y);
+std::shared_ptr<IntStream> add(IntStream& x, IntStream& y);
 
 // x - y
-IntStream sub(IntStream x, IntStream y);
+std::shared_ptr<IntStream> sub(IntStream& x, IntStream& y);
 
 // x * y
-IntStream mul(IntStream x, IntStream y);
+std::shared_ptr<IntStream> mul(IntStream& x, IntStream& y);
 
 // x / y
-IntStream div(IntStream x, IntStream y);
+std::shared_ptr<IntStream> div(IntStream& x, IntStream& y);
 
 // x % y
-IntStream mod(IntStream x, IntStream y);
+std::shared_ptr<IntStream> mod(IntStream x, IntStream& y);
 
 #endif //GPU_TESSLA_STREAM_FUNCTIONS_H
