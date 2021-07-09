b5464cfbdd1b3e6c490588980d799951d912b531
TheBitFighter <fabian@theposch.dev>
2021-07-09 18:21:16 +0200

    [CUDA] Added divide and inverted divide operation

--- arc/tessla_cuda/src/ImmediateFunctions.cuh
+++ arc/tessla_cuda/src/ImmediateFunctions.cuh
@@ -16,11 +16,15 @@ shared_ptr<GPUIntStream> add_imm(shared_ptr<GPUIntStream> input, size_t imm, cud
 shared_ptr<GPUIntStream> mul_imm (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
 shared_ptr<GPUIntStream> sub_imm (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
 shared_ptr<GPUIntStream> sub_inv_imm (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
+shared_ptr<GPUIntStream> div_imm (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
+shared_ptr<GPUIntStream> div_inv_imm (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
 
 // CUDA operations. Do not use externally
 __global__ void add_imm_cuda (const int *input_val, int imm, int *output_val);
 __global__ void mul_imm_cuda (const int *input_val, int imm, int *output_val);
 __global__ void sub_imm_cuda (const int *input_val, int imm, int *output_val);
 __global__ void sub_inv_imm_cuda (const int *input_val, int imm, int *output_val);
+__global__ void div_imm_cuda (const int *input_val, int imm, int *output_val);
+__global__ void div_inv_imm_cuda (const int *input_val, int imm, int *output_val);
 
 #endif //ARC_IMMEDIATEFUNCTIONS_CUH
