9271e1e7d0220f67bc7c47ee24d3fc79ca153e05
Klaus Kraßnitzer <klaus.krassnitzer@student.tuwien.ac.at>
2021-07-10 16:11:06 +0200

    thrust immeadiate header

---
+++ arc/tessla_cuda/src/ImmediateFunctionsThrust.cu
@@ -0,0 +1,26 @@
+//
+// Created by klaus on 09.07.21.
+//
+
+#include "ImmediateFunctionsThrust.cuh"
+#include "StreamFunctionHelper.cuh"
+#include <thrust/transform.h>
+#include <thrust/functional.h>
+
+// Calling interface
+shared_ptr<GPUIntStream> add_imm_thrust(shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream) {
+
+}
+
+shared_ptr<GPUIntStream> mul_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream) {
+
+}
+
+shared_ptr<GPUIntStream> sub_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream) {
+
+}
+
+shared_ptr<GPUIntStream> sub_inv_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream) {
+
+}
+
