ea9c8702f912b5696a335d6c2f5e46a9f580ef46
danielschloms <daniel.schloms@gmail.com>
2021-07-13 08:49:04 +0000

    Merge sequential benchmarking

--- arc/tessla_seq/test/test_benchmark_seq.cpp
+++ arc/tessla_seq/test/test_benchmark_seq.cpp
@@ -189,4 +189,44 @@ TEST_CASE("BENCHMARKING SEQUENTIAL") {
         }
     }
 
+    SECTION("lift() merge benchmarking, sequential version") {
+        std::ofstream output_last;
+        //delete previous
+        output_last.open("benchmarking_lift_merge_seq.data");
+        output_last << "";
+        output_last.close();
+
+        for (int j = 1; j <= BENCHMARKING_LOOPS; j++){
+            for (int i = 1; i <= 5; i++) {
+                auto start2 = std::chrono::high_resolution_clock::now();
+
+                std::string path = "test/data/benchmarking";
+                Reader inReader = Reader(path+std::to_string(i)+"_lift.in");
+                Reader outReader = Reader(path+std::to_string(i)+"_slift_merge.out");
+
+                std::shared_ptr<IntStream> inputStreamZ = inReader.getIntStream("z");
+                std::shared_ptr<IntStream> inputStreamY = inReader.getIntStream("y");
+                std::shared_ptr<IntStream> CORRECT_STREAM = outReader.getIntStream("x");
+
+                int size = sizeof(*CORRECT_STREAM) / sizeof(int);
+
+                auto start = std::chrono::high_resolution_clock::now();
+
+                std::shared_ptr<IntStream> result = merge(*inputStreamZ, *inputStreamY);
+
+                auto stop = std::chrono::high_resolution_clock::now();
+
+                #ifdef CHECK_RESULTS
+                REQUIRE(*result == *CORRECT_STREAM);
+                #endif
+
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+                auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start2);
+                output_last.open ("benchmarking_lift_merge_seq.data",std::ios::app);
+                output_last << "Benchmark " << i << ": " << duration.count()
+                            << " us" << " with reader: " << duration2.count()<< " us size: " << size << "\n";
+                output_last.close();
+            }
+        }
+    }
 }
