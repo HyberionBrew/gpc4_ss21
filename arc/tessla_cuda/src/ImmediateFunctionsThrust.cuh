4e2b921380cf356c561c84ec387c2b8722961568
danielschloms <daniel.schloms@gmail.com>
2021-07-13 01:06:31 +0000

    Fix Thrust scheduler compilation

--- arc/tessla_cuda/src/ImmediateFunctionsThrust.cuh
+++ arc/tessla_cuda/src/ImmediateFunctionsThrust.cuh
@@ -12,13 +12,13 @@
 using namespace std;
 
 // Calling interface
-shared_ptr<GPUIntStream> add_imm_thrust(shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
-shared_ptr<GPUIntStream> mul_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
-shared_ptr<GPUIntStream> sub_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
-shared_ptr<GPUIntStream> sub_inv_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
-shared_ptr<GPUIntStream> div_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
-shared_ptr<GPUIntStream> div_inv_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
-shared_ptr<GPUIntStream> mod_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
-shared_ptr<GPUIntStream> mod_inv_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm, cudaStream_t stream);
+shared_ptr<GPUIntStream> add_imm_thrust(shared_ptr<GPUIntStream> input, size_t imm);
+shared_ptr<GPUIntStream> mul_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm);
+shared_ptr<GPUIntStream> sub_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm);
+shared_ptr<GPUIntStream> sub_inv_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm);
+shared_ptr<GPUIntStream> div_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm);
+shared_ptr<GPUIntStream> div_inv_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm);
+shared_ptr<GPUIntStream> mod_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm);
+shared_ptr<GPUIntStream> mod_inv_imm_thrust (shared_ptr<GPUIntStream> input, size_t imm);
 
 #endif //ARC_IMMEDIATEFUNCTIONSTHRUST_CUH
