28c7978247600b05eb43426f78e258dcffad692c
danielschloms <daniel.schloms@gmail.com>
2021-07-14 08:25:47 +0000

    Thrust lift() benchmarking cases

--- arc/tessla_cuda/test/test_benchmark_thrust.cpp
+++ arc/tessla_cuda/test/test_benchmark_thrust.cpp
@@ -210,4 +210,141 @@ TEST_CASE("BENCHMARKING THRUST") {
             }
         }
     }
+
+    SECTION("time() benchmarking, thrust version") {
+        std::ofstream output_last;
+        output_last.open("benchmarking_time_thrust.data");
+        output_last << "";
+        output_last.close();
+
+        int dev = 0;
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+
+        printf("Using Device %d: %s\n", dev, deviceProp.name);
+        for (int j = 1; j <= BENCHMARKING_LOOPS; j++) {
+            cudaDeviceSynchronize();
+            for (int i = 1; i <= BENCHMARKING_CASES; i++) {
+                auto start2 = std::chrono::high_resolution_clock::now();
+
+                std::string path = "test/data/benchmarking";
+                GPUReader inReader = GPUReader(path + std::to_string(i) + ".in");
+                GPUReader outReader = GPUReader(path + std::to_string(i) + "_time.out");
+
+                std::shared_ptr<GPUIntStream> inputStreamZ = inReader.getIntStream("z");
+                std::shared_ptr<GPUIntStream> CORRECT_STREAM = outReader.getIntStream("y");
+
+                int size = CORRECT_STREAM->size;
+                auto start = std::chrono::high_resolution_clock::now();
+
+                std::shared_ptr<GPUIntStream> outputStream;
+
+                inputStreamZ->copy_to_device();
+
+                outputStream = time_thrust(inputStreamZ);
+                outputStream->copy_to_host();
+
+                cudaDeviceSynchronize();
+
+                auto stop = std::chrono::high_resolution_clock::now();
+
+                std::vector<int> kernelTimestamps(outputStream->host_timestamp + *(outputStream->host_offset), outputStream->host_timestamp + outputStream->size);
+                std::vector<int> kernelValues(outputStream->host_values + *(outputStream->host_offset), outputStream->host_values + outputStream->size);
+                std::vector<int> correctTimestamps(CORRECT_STREAM->host_timestamp, CORRECT_STREAM->host_timestamp + CORRECT_STREAM->size);
+                std::vector<int> correctValues(CORRECT_STREAM->host_values, CORRECT_STREAM->host_values + CORRECT_STREAM->size);
+
+                REQUIRE(kernelTimestamps == correctTimestamps);
+                REQUIRE(kernelValues == correctValues);
+
+                // Cleanup
+                inputStreamZ->free_device();
+                outputStream->free_device();
+                inputStreamZ->free_host();
+                outputStream->free_host();
+
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+                auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start2);
+                output_last.open("benchmarking_time_thrust.data", std::ios::app);
+                output_last << "Benchmark " << i << ": " << duration.count()
+                            << " us" << " with reader: " << duration2.count()<< " us size: " << size << "\n";
+                output_last.close();
+            }
+        }
+    }
+
+    SECTION("lift() merge benchmarking, thrust version") {
+        std::ofstream output_last;
+        output_last.open("benchmarking_lift_merge_thrust.data");
+        output_last << "";
+        output_last.close();
+
+        int dev = 0;
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+
+        printf("Using Device %d: %s\n", dev, deviceProp.name);
+        for (int j = 1; j <= BENCHMARKING_LOOPS; j++) {
+            cudaDeviceSynchronize();
+            for (int i = 1; i <= 5; i++) {
+                auto start2 = std::chrono::high_resolution_clock::now();
+
+                std::string path = "test/data/benchmarking";
+                GPUReader inReader = GPUReader(path + std::to_string(i) + "_lift.in");
+                GPUReader outReader = GPUReader(path + std::to_string(i) + "_slift_merge.out");
+
+                std::shared_ptr<GPUIntStream> inputStreamZ = inReader.getIntStream("z");
+                std::shared_ptr<GPUIntStream> inputStreamY = inReader.getIntStream("y");
+                std::shared_ptr<GPUIntStream> CORRECT_STREAM = outReader.getIntStream("x");
+
+                int size = CORRECT_STREAM->size;
+                auto start = std::chrono::high_resolution_clock::now();
+
+                std::shared_ptr<GPUIntStream> outputStream;
+                inputStreamZ->copy_to_device();
+                inputStreamY->copy_to_device();
+
+                outputStream = slift_thrust(inputStreamZ, inputStreamY, TH_OP_merge, 0);
+                outputStream->copy_to_host();
+
+                cudaDeviceSynchronize();
+
+                auto stop = std::chrono::high_resolution_clock::now();
+
+                std::vector<int> kernelTimestamps(outputStream->host_timestamp + *(outputStream->host_offset), outputStream->host_timestamp + outputStream->size);
+                std::vector<int> kernelValues(outputStream->host_values + *(outputStream->host_offset), outputStream->host_values + outputStream->size);
+                std::vector<int> correctTimestamps(CORRECT_STREAM->host_timestamp, CORRECT_STREAM->host_timestamp + CORRECT_STREAM->size);
+                std::vector<int> correctValues(CORRECT_STREAM->host_values, CORRECT_STREAM->host_values + CORRECT_STREAM->size);
+
+                //REQUIRE(kernelTimestamps[2] == correctTimestamps[2]);
+                /*
+                for (int i = 0; i < 50; i++ ) {
+                    printf("%i, ", kernelTimestamps[i]);
+                }
+                printf("\n");
+                for (int i = 0; i < 50; i++ ) {
+                    printf("%i, ", kernelValues[i]);
+                }
+                printf("\n");
+                */
+                REQUIRE(kernelTimestamps == correctTimestamps);
+                REQUIRE(kernelValues == correctValues);
+
+                // Cleanup
+                inputStreamZ->free_device();
+                inputStreamY->free_device();
+                outputStream->free_device();
+                inputStreamZ->free_host();
+                inputStreamY->free_host();
+                outputStream->free_host();
+
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+                auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start2);
+                output_last.open("benchmarking_lift_merge_thrust.data", std::ios::app);
+                output_last << "Benchmark " << i << ": " << duration.count()
+                            << " us" << " with reader: " << duration2.count()<< " us size: " << size << "\n";
+                output_last.close();
+            }
+        }
+    }
+
 }
